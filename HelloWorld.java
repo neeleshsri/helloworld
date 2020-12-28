ef8cf0f53bccf81c71dd8973e730f88dbad3b0d5
neeleshsri <76560169+neeleshsri@users.noreply.github.com>
2020-12-28 14:47:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-            System.out.println("Hello to java"); 
+            System.out.println("Hello hi to java"); 
     }
 }
